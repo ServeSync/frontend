f98db9fc3a1b0b800526160a0d9e698f6c91378a
= <=>
2023-11-24 22:53:36 +0700

    fix: fix validate form time

--- src/modules/StudentManagement/utils/rules/export.rules.ts
+++ src/modules/StudentManagement/utils/rules/export.rules.ts
@@ -2,13 +2,13 @@ import * as yup from 'yup'
 
 export const FormExportFileSchema = yup.object().shape({
   formDate: yup.string(),
-  toDate: yup.string().test('minTime', 'Thời gian kết thúc phải sau thời gian bắt đầu', function (endAt) {
-    const startAt = this.parent.startAt
-    if (!startAt || !endAt) {
+  toDate: yup.string().test('successTime', 'Thời gian kết thúc phải sau thời gian bắt đầu', function (toDate) {
+    const formDate = this.parent.formDate
+    if (!formDate || !toDate) {
       return true
     }
-    const startDate = new Date(startAt)
-    const endDate = new Date(endAt)
+    const startDate = new Date(formDate)
+    const endDate = new Date(toDate)
     return endDate > startDate
   })
 })
