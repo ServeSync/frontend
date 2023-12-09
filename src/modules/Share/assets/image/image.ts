a76dea04b52b7fd756c5c95f4ec94e0eeda96fc2
Ngocnguyn <ngocnguyen752002@gmail.com>
2023-12-09 18:52:22 +0700

    fix: upload home image

--- src/modules/Share/assets/image/image.ts
+++ src/modules/Share/assets/image/image.ts
@@ -1,11 +1,11 @@
 export const logo = 'https://res.cloudinary.com/dboijruhe/image/upload/v1698472046/Assets/mqdwwsszosbceacvw4eu.png'
 export const global_image =
-  'http://res.cloudinary.com/dboijruhe/image/upload/v1699333104/ServeSync/34100fad-14a0-4f16-9038-b2d6d98b0858-8d0b3da488e360fcb93103240156f714.png'
+  'http://res.cloudinary.com/dboijruhe/image/upload/v1702117547/ServeSync/8959ecd9-7167-491e-89a3-11f5bdafbd45-8d0b3da488e360fcb93103240156f714_1.png'
 export const homePage_01 =
   'http://res.cloudinary.com/dboijruhe/image/upload/v1699333500/ServeSync/6aa0528c-287e-4baa-8fda-fdc77d018813-1.jpg'
 export const homePage_02 =
   'http://res.cloudinary.com/dboijruhe/image/upload/v1699333667/ServeSync/d8c0e09d-140b-411b-865c-c3b37a6e4e43-2.png'
 export const homePage_03 =
-  'http://res.cloudinary.com/dboijruhe/image/upload/v1699333697/ServeSync/0f92b622-1681-452c-ba72-3060198b1a64-3.png'
+  'http://res.cloudinary.com/dboijruhe/image/upload/v1702122653/ServeSync/a521fc80-16a4-4229-9570-a2b637f97576-0ba1ef31dd9cebc579284487687bfbcc_1.jpg'
 export const listEventPage =
   'https://res.cloudinary.com/dboijruhe/image/upload/v1699351883/ServeSync/d2ec6236-d90a-4124-baa1-ccfee7bbdc61-672aeee8e6b4b141ef3e851b6fb3fa79.png'
