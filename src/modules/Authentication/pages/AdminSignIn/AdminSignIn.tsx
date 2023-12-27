caa9f95a54368d77542b29e7ef0ba703771160d3
Ngocnguyn <ngocnguyen752002@gmail.com>
2023-12-27 11:33:21 +0700

    update: response UI page login user and admin

--- src/modules/Authentication/pages/AdminSignIn/AdminSignIn.tsx
+++ src/modules/Authentication/pages/AdminSignIn/AdminSignIn.tsx
@@ -47,17 +47,25 @@ const AdminSignIn = () => {
         <title>Login</title>
         <meta name='description' content='This is login page of the project' />
       </Helmet>
-      <div className='grid grid-cols-2 bg-white rounded-3xl w-[1000px] overflow-hidden shadow-[rgba(25,_94,_142,_0.36)_2px_9px_20px]'>
-        <div className='col-span-1 flex items-center justify-center bg-[#bdeef4] rounded-e-full'>
-          <img src={global_image} alt='image_login' className='mx-auto w-[300px]' />
+      <div className='grid grid-cols-2 bg-white rounded-3xl lg:w-[1000px] md:w-[700px] max-md:w-[320px] overflow-hidden shadow-[rgba(25,_94,_142,_0.36)_2px_9px_20px]'>
+        <div className='col-span-1 px-10 pt-10 bg-[#bdeef4] rounded-e-full flex items-center justify-center'>
+          <img src={global_image} alt='image_login' className='md:w-[300px] max-md:w-[150px]' />
         </div>
-        <div className='col-span-1 p-12 flex flex-col justify-center'>
+        <div className='col-span-1 lg:px-10 lg:py-20 md:px-5 md:py-10 max-md:px-4 max-md:py-6 flex flex-col justify-center'>
           <Link to={path.home_page} className='flex justify-center mb-4'>
-            <img src={logo} alt='logo' className='w-[80px] h-[80px]' />
+            <img
+              src={logo}
+              alt='logo'
+              className='lg:w-[80px] lg:h-[80px] md:w-[60px] md:h-[60px] max-md:w-[40px] max-md:h-[40px]'
+            />
           </Link>
-          <div className='mb-[20px] text-center'>
-            <h1 className='text-[40px] text-[#195E8E] font-bold mb-[10px]'>Đăng nhập</h1>
-            <h2 className='text-[#195E8E]'>Hệ thống quản lý hoạt động cộng đồng.</h2>
+          <div className='md:mb-[20px] max-md:mb-[10px] text-center'>
+            <h1 className='lg:text-[40px] md:text-[30px] max-md:text-[18px] text-[#195E8E] font-bold mb-[10px]'>
+              Đăng nhập
+            </h1>
+            <h2 className='text-[#195E8E] lg:text-[16px] max-md:text-[12px] sm:text-[10px]'>
+              Hệ thống quản lý hoạt động cộng đồng.
+            </h2>
           </div>
           <form onSubmit={handleSubmitForm}>
             <AdminSignInForm register={register} errors={errors} isLoading={adminSignInCommandHandler.isLoading()} />
